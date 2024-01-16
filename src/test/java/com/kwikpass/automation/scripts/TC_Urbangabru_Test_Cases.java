38caa364b85bde1bbd34ac31e5126cd2072f4787
unakalprimathon <naveen.unakal@primathon.in>
2024-01-16 16:38:40 +0530

    added new merchant Test case

---
+++ src/test/java/com/kwikpass/automation/scripts/TC_Urbangabru_Test_Cases.java
@@ -0,0 +1,7 @@
+package com.kwikpass.automation.scripts;
+
+import com.kwikpass.pageObjects.BaseClass;
+import com.kwikpass.pageObjects.Locator;
+
+public class TC_Urbangabru_Test_Cases extends BaseClass implements Locator {
+}
