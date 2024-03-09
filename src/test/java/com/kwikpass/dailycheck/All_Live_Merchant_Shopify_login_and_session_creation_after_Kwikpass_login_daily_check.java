23d1fb3e64767f66c27f0b98d20104d83c48c7ac
unakalprimathon <naveen.unakal@primathon.in>
2024-03-09 22:02:48 +0530

    Updated daily sanity scripts

--- src/test/java/com/kwikpass/dailycheck/All_Live_Merchant_Shopify_login_and_session_creation_after_Kwikpass_login_daily_check.java
+++ src/test/java/com/kwikpass/dailycheck/All_Live_Merchant_Shopify_login_and_session_creation_after_Kwikpass_login_daily_check.java
@@ -3166,4 +3166,47 @@ public class All_Live_Merchant_Shopify_login_and_session_creation_after_Kwikpass
 			Assert.fail("Verify_Stickitup_Shopify_login_and_session_creation_after_Kwikpass_login failed");
 		}
 	}
+	
+	@Test
+	public void Verify_Karagiri_KwikPass_Login_at_accountpage() throws IOException, InterruptedException {
+		logger.info("Verify_Karagiri_KwikPass_Login_at_accountpage Test case is running.....");
+		driver.get("https://www.karagiri.com/account");
+		logger.info("URL is open");
+		logger.info("Karagiri doesn't has Kwikpass AP");
+		LoginPage lp = new LoginPage(driver);
+		sleep(3);
+		lp.switchToIframe(kwikpass_iframe);
+		logger.info("switched to Kwikpass login modal iframe");
+		By mobile_input_field = By.id(mobile_input);
+		By whatsapp_btn = By.id(whatsappid);
+		boolean mobile_input_fieldpresent = !driver.findElements(mobile_input_field).isEmpty();
+		boolean whatsapp_btnpresent = !driver.findElements(whatsapp_btn).isEmpty();
+		if (mobile_input_fieldpresent && whatsapp_btnpresent) {
+			logger.info("The Kwikpass AP modal has OTP via SMS and WhatsApp login options.");
+			driver.findElement(By.id(mobile_input)).sendKeys(mobile_number);
+			logger.info("Mobile number entered");
+			sleep(2);
+			driver.findElement(By.id(otp_input)).sendKeys(otp);
+			logger.info("OTP entered");
+			sleep(8);
+			driver.switchTo().defaultContent();
+			String curl = driver.getCurrentUrl();
+			String vurl = "https://www.karagiri.com/account";
+			if (curl.equals(vurl)) {
+				driver.findElement(By.xpath(acc_logout_btn)).isDisplayed();
+				logger.info("The Kwikpass Login successfull.");
+				logger.info("Verify_Karagiri_KwikPass_Login_at_accountpage Test Cases passed!");
+				logger.info("Test Cases Completed !");
+			} else {
+				logger.info("Verify_Karagiri_KwikPass_Login_at_accountpage Test Case failed!");
+				captureScreen(driver,"Verify_Karagiri_KwikPass_Login_at_accountpage failed");
+				Assert.fail("Verify_Karagiri_KwikPass_Login_at_accountpage failed");
+			}
+		} else {
+			logger.info("The Kwikpass Login modal doesn't has OTP via SMS or WhatsApp login options.");
+			logger.info("Verify_Karagiri_KwikPass_Login_at_accountpage Test Cases failed!");
+			captureScreen(driver, "Verify_Karagiri_KwikPass_Login_at_accountpage failed");
+			Assert.fail("Verify_Karagiri_KwikPass_Login_at_accountpage failed");
+		}
+	}
 }
